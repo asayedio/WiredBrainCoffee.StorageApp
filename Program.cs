237611fe1a282809f3389272bbaf1656ec4b49c7
Ahmed Sayed <ahmedsayed.9191@gmail.com>
2021-08-08 23:40:39 +0200

    Inherit from generic class

--- Program.cs
+++ Program.cs
@@ -9,17 +9,33 @@ namespace WiredBrainCoffee.StorageApp
         static void Main(string[] args)
         {
             var employeeRepository = new GenericRepository<Employee>();
-            employeeRepository.Add(new Employee{ FirstName = "Ahmed" });
+            AddEmployees(employeeRepository);
+            GetEmployeeById(employeeRepository);
+
+            var organizationRepository = new GenericRepository<Organization>();
+            AddOrganizations(organizationRepository);
+            Console.ReadLine();
+        }
+
+        private static void GetEmployeeById(GenericRepository<Employee> employeeRepository)
+        {
+            var employee = employeeRepository.GetById(2);
+            Console.WriteLine($"The employee Id: {employee.Id} & first name is: {employee.FirstName}");
+        }
+
+        private static void AddEmployees(GenericRepository<Employee> employeeRepository)
+        {
+            employeeRepository.Add(new Employee { FirstName = "Ahmed" });
             employeeRepository.Add(new Employee { FirstName = "Sayed" });
             employeeRepository.Add(new Employee { FirstName = "Sobhy" });
             employeeRepository.Save();
-
-            var organizationRepository = new GenericRepository<Organization>();
+        }
+        private static void AddOrganizations(GenericRepository<Organization> organizationRepository)
+        {
             organizationRepository.Add(new Organization { Name = "GLC" });
             organizationRepository.Add(new Organization { Name = "CELEPTA" });
             organizationRepository.Add(new Organization { Name = "COCACOLA" });
             organizationRepository.Save();
-            Console.ReadLine();
         }
     }
 }
