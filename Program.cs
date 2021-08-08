340505e8552b6042f5f627a0e386f17050187eb5
Ahmed Sayed <ahmedsayed.9191@gmail.com>
2021-08-08 20:05:15 +0200

    Implement generic repository class

--- Program.cs
+++ Program.cs
@@ -8,11 +8,17 @@ namespace WiredBrainCoffee.StorageApp
     {
         static void Main(string[] args)
         {
-            var employeeRepository = new EmployeeRepository();
+            var employeeRepository = new GenericRepository<Employee>();
             employeeRepository.Add(new Employee{ FirstName = "Ahmed" });
             employeeRepository.Add(new Employee { FirstName = "Sayed" });
             employeeRepository.Add(new Employee { FirstName = "Sobhy" });
             employeeRepository.Save();
+
+            var organizationRepository = new GenericRepository<Organization>();
+            organizationRepository.Add(new Organization { Name = "GLC" });
+            organizationRepository.Add(new Organization { Name = "CELEPTA" });
+            organizationRepository.Add(new Organization { Name = "COCACOLA" });
+            organizationRepository.Save();
             Console.ReadLine();
         }
     }
---
+++ Repositories/GenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace WiredBrainCoffee.StorageApp.Repositories
+{
+    public class GenericRepository<T>
+    {
+        private readonly List<T> _itmes = new();
+        public void Add(T item)
+        {
+            if (item != null)
+                _itmes.Add(item);
+        }
+
+        public void Save()
+        {
+            foreach (var item in _itmes)
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+    }
+}
